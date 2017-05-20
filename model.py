9d0d95eef7911910846e1bc6068f1c0d66104ed5
Nahom Abi <nahomagidew@gmail.com>
2017-05-20 15:53:09 -0700

    Fixed training bug by reformatting pandas array.

--- model.py
+++ model.py
@@ -21,8 +21,8 @@ np.random.seed(0)
 # df.get_values()[:, :3]
 # df.get_values()[:, 4:5]
 data_df = pd.read_csv('./driving_log.csv')
-X = data_df[['center', 'left', 'right']].values
-y = data_df['steering'].values
+X = data_df.get_values()[:, :3]
+y = data_df.get_values()[:, 4:5]
 
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.20, random_state=0)
 
@@ -46,11 +46,11 @@ X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.20, rand
 """
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=INPUT_SHAPE))
-model.add(Conv2D(24, (5, 5), activation='elu', subsample=(2, 2)))
-model.add(Conv2D(36, (5, 5), activation='elu', subsample=(2, 2)))
-model.add(Conv2D(48, (5, 5), activation='elu', subsample=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='elu'))
-model.add(Conv2D(64, (3, 3), activation='elu'))
+model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+model.add(Conv2D(64, 3, 3, activation='elu'))
+model.add(Conv2D(64, 3, 3, activation='elu'))
 model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100, activation='elu'))
